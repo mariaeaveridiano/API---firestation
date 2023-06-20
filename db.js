8fa05cd0515d5f8f77546c2022964678be7e6b19
mariaeaveridiano <ma058589@intelbras.com>
2023-06-20 17:13:50 -0300

    comitas

--- db.js
+++ db.js
@@ -1,20 +1,22 @@
+const { PROTOCOL } = require('sqlite3');
+
 const sqlite3 = require('sqlite3').verbose();
 // import sqlite3 from ('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 db.serialize(() => {
-    db.run("CREATE TABLE parameters(qtd_eventos INT, ip TEXT, porta TEXT, endpoint TEXT)");
+    db.run("CREATE TABLE parameters(qnt_eventos INT, ip TEXT, porta TEXT, endpoint TEXT)");
 
 });
 
-function insertParameters(table, qtd_eventos, ip, porta, endpoint) {
-    const query = `INSERT INTO ${table} values(${qtd_eventos}, "${ip}", "${porta}", "${endpoint}")`
+function insertParameters(table, qnt_eventos, ip, porta, endpoint) {
+    const query = `INSERT INTO ${table} values("${qnt_eventos}", "${ip}", "${porta}", "${endpoint}")`
     console.log(query)
-    const alaolao = db.run (query)
-    console.log(alaolao)
+    const inserindo = db.run (query)
+    console.log(inserindo)
 
 }
 async function selectParameters_all() {
-    let query =`SELECT * FROM parameters`
+    const query =`SELECT * FROM parameters`
         return new Promise(function(resolve,reject){
             db.all(query, function(err,rows){
                if(err){return reject(err);}
@@ -24,7 +26,7 @@ async function selectParameters_all() {
     }
 
 async function selectParameters_ip(ip){
-    let query = `SELECT ip FROM parameters`
+    const query = `SELECT * FROM parameters WHERE ip="${ip}"`
     return new Promise(function(resolve,reject){
         db.all(query, function(err,rows){
            if(err){return reject(err);}
@@ -33,20 +35,25 @@ async function selectParameters_ip(ip){
     });
 }
 
-function updateParameters(table, qtd_eventos, ip, porta, endpoint) {
-    let query = `INSERT INTO ${table} values(${qtd_eventos}, "${ip}", "${porta}", "${endpoint}")`
-    console.log(query)
-    const uptando = db.run (query)
-    console.log(uptando)
-
+function updateParameters(qnt_eventos, ip, porta, endpoint) {
+    console.log(qnt_eventos)
+    console.log(ip)
+    console.log(porta)
+    console.log(endpoint)
+    let query = `UPDATE parameters SET porta = ${porta}, qnt_eventos =${qnt_eventos}, endpoint = "${endpoint}" WHERE ip = "${ip}"`
+    return new Promise(function(resolve,reject){
+    db.all(query, function(err,rows){
+        if(err){return reject(err);}
+        resolve(rows);
+    });
+      });
 }
-function deleteParameters(table, qtd_eventos, ip, porta, endpoint) {
-    let query =`INSERT INTO ${table} values(${qtd_eventos}, "${ip}", "${porta}", "${endpoint}")`
+
+function deleteParameters(ip) {
+    let query = `DELETE FROM parameters WHERE ip ="${ip}"`
     console.log(query)
     const deletando = db.run (query)
     console.log(deletando)
-
 }
-
  
-module.exports = {insertParameters, selectParameters_ip, selectParameters_all, updateParameters, deleteParameters }
+module.exports = {insertParameters, selectParameters_ip, selectParameters_all,updateParameters, deleteParameters }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser'); 
 const db = require("./db");
 const app = express();
+ const axios = require ('axios');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -9,19 +10,91 @@ app.use(bodyParser.json());
 let servidores = [];
 
 setInterval(async() => {
-let qnt_eventos
 const select_tudo = await db.selectParameters_all()
 console.log(select_tudo)
-}, 1000);
+}, 10000);
 
 setInterval(async() => {
-    let qnt_eventos
     const select_ip = await db.selectParameters_ip()
     console.log(select_ip)
-    }, 1000);
+    }, 10000);
 
-// import {deleteParameters, updateParameters, selectParameters, insertParameters} from './db.js'
-
+ function enviarpost (qnt_eventos,endpoint,porta,ip){
+        body= {
+        "qnt_eventos":5,
+        "endpoint":"/alura",
+        "porta":58,
+        "ip":"10.100.111.02",
+        "SchemaVersion": "1.0",
+        "Event": {
+        "Type": "GW005",
+        "DateTime": "09/09/21 15:25:47",
+        "Seq": 1383,
+        "GlobalSeq": 13333,
+        "Zone": 0,
+        "ZoneName": "",
+        "Loop": 0,
+        "Device": 11,
+        "DeviceType": "-",
+        "DeviceName": "DF APTO 401",
+        "Extract": "Alarme – 4 Andar DF APTO 401"
+        },
+        "General": {
+        "UserCode": 1234,
+        "Gw521Version": "0.0.9",
+        "CieVersion": "3.1.8",
+        "Alarms": 1,
+        "Supervisions": 0,
+        "Failures": 0,
+        "MacAddr": "00:04:25:1C:A1:0A"
+        }
+       }}
+       axios({
+        method:'post',
+        url: 'http://localhost:3000/servidores',
+        data:{
+            "qnt_eventos":5,
+            "endpoint":"/alura",
+            "porta":58,
+            "ip":"10.100.111.02",
+            "SchemaVersion": "1.0",
+            "Event": {
+            "Type": "GW005",
+            "DateTime": "09/09/21 15:25:47",
+            "Seq": 1383,
+            "GlobalSeq": 13333,
+            "Zone": 0,
+            "ZoneName": "",
+            "Loop": 0,
+            "Device": 11,
+            "DeviceType": "-",
+            "DeviceName": "DF APTO 401",
+            "Extract": "Alarme – 4 Andar DF APTO 401"
+            },
+            "General": {
+            "UserCode": 1234,
+            "Gw521Version": "0.0.9",
+            "CieVersion": "3.1.8",
+            "Alarms": 1,
+            "Supervisions": 0,
+            "Failures": 0,
+            "MacAddr": "00:04:25:1C:A1:0A"
+            }
+           },
+           headers:{}
+    }).then((response)=>{
+        console.log(response.status)
+    }).catch((error)=>{
+        console.log(error)
+    })
+    console.log (enviarpost)
+ 
+    
+    for (let i = 0; i < `${qnt_eventos}`; i++) {
+        console.log(i)
+        
+    }
+           
 app.get('/servidores', (req, res) => { //mandar 204 vazio
     db.selectParameters_all("parameters")
     res.status(200).json(servidores);
@@ -43,19 +116,19 @@ app.post('/servidores', (req, res) => {
     if (qnt_eventos == null) {
         res.status(400).json({ message: 'quantidade de eventos nula' })
         return;
-    } // melhorar logica 
+    } 
     if (ip == null) {
         res.status(400).json({ message: 'ip nulo' })
         return;
-    } // melhorar logica
+    } 
     if (porta == null) {
         res.status(400).json({ message: 'porta nula' })
         return;
-    } // melhorar logica
+    } 
     if (endpoint == null) {
         res.status(400).json({ message: 'endpoint nulo' })
         return;
-    } // melhorar logica
+    } 
     db.insertParameters("parameters", qnt_eventos, ip, porta, endpoint)
     for (let i = 0; i < servidores.length; i++) {
         const servidor = servidores[i];
@@ -73,7 +146,7 @@ app.post('/servidores', (req, res) => {
 app.put('/servidores/:ip', (req, res) => {
     const { ip } = req.params;
     const { qnt_eventos, porta, endpoint } = req.body;
-    db.updateParameters("parameters", qnt_eventos, ip, porta, endpoint)
+    db.updateParameters(qnt_eventos, ip, porta, endpoint)
     const servidor = servidores.find(servidor => servidor.ip == ip);
     if (servidor) {
         servidor.qnt_eventos = qnt_eventos || servidor.qnt_eventos;
@@ -83,11 +156,11 @@ app.put('/servidores/:ip', (req, res) => {
         res.status(202).json({ menssage: 'informações do servidor atualizadas com sucesso' });
     } else {
         res.status(404).json({ message: 'servidor não encontrado.' });
-    }
+    }});
 
     app.delete('/servidores/:ip', (req, res) => {
         const { ip } = req.params;
-        db.deleteParameters("parameters", ip)
+        db.deleteParameters(ip)
         const servidorIndex = servidores.findIndex(servidor => servidor.ip == ip);
         if (servidorIndex !== -1) {
             servidores.splice(servidorIndex, 1);
@@ -96,7 +169,7 @@ app.put('/servidores/:ip', (req, res) => {
             res.status(404).json({ message: 'servidor não encontrado' });
         }
     })
-});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`servidor rodando em http://localhost:${port}`);
